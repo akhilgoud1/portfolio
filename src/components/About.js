c2cee9d0f0758a5f36e55dec46ab05e7bdd3d564
akhil <akhilgoud9399@gmail.com>
2024-03-10 11:22:04 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section id="about">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            H<span className="text-red-700">i</span>, I<span className="text-red-700">'</span>m Akhil.
+            <br className="hidden lg:inline-block" /> I love to build amazing
+            apps and Websites.
+          </h1>
+          <p className="mb-8 leading-relaxed">
+          I am passionate about crafting dynamic and responsive web applications using MongoDB, Express.js, React.js, and Node.js. Eager to contribute 
+          my skills in front-end and back-end development to create seamless user experiences and equipped with a solid understanding of RESTful APIs. 
+          </p>
+          <div className="flex justify-center">
+            <a
+              href="#contact"
+              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+              Hire Me
+            </a>
+            <a
+              href="#projects"
+              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+              Projects
+            </a>
+          </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center rounded"
+            alt="hero"
+            src="./coding.svg"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,61 @@
+export const projects = [
+  {
+    title: "Dynamic Portfolio Website Development Project",
+    subtitle: "React",
+    description:
+      "I spearheaded the creation of a dynamic and visually engaging portfolio website, demonstrating my proficiency in web development and design. This project showcases my commitment to creating an online presence that is both aesthetically pleasing and functionally impressive",
+    image: "./screen-capture2-ezgif.com-video-to-gif-converter.gif",
+    link: "",
+  },
+  {
+    title: "Food Ordering Platform Development Project",
+    subtitle: "MERN Stack",
+    description:
+      "I led the development of a robust and user-friendly food ordering website, showcasing my expertise in web development and e-commerce solutions. This project underscores my ability to deliver intuitive and efficient online platforms that meet user needs and enhance business operations.",
+    image: "./screen-capture1-ezgif.com-video-to-gif-converter.gif",
+    link: "",
+  },
+  {
+    title: "FullStack CRUD Apllication",
+    subtitle: "React Hooks",
+    description:
+      "The application, designed to manage tasks efficiently, featured a modern and intuitive user interface developed using React.js. The back-end was powered by Node.js and Express, ensuring secure user authentication and seamless data flow.",
+    image: "./final.gif",
+    link: "",
+  },
+  {
+    title: "Ecommerce Application",
+    subtitle: "MERN STACK",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./giphy.webp",
+    link: "",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node js",
+  "Tailwind CSS",
+  "HTML",
+  "MongoDB"
+];
